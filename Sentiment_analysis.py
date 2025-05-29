219556b38675fc5256309e381aecfcd8e84f30b2
Areeba Akram <134126927+AreebaAkram@users.noreply.github.com>
2025-05-29 22:09:59 +0500

    Add files via upload
    Streamlit base sentiment analysis using Python

---
+++ Sentiment_analysis.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from textblob import TextBlob
+
+st.title("Sentiment analysis")
+text = st.text_area("enter your comment about the product")
+blob = TextBlob(text)
+polarity = blob.sentiment.polarity
+subjectivity = blob.sentiment.subjectivity
+
+if st.button("Check polarity"):
+    st.write(polarity)
+    if (polarity > 0):
+        st.write("Positive Comment")
+    elif( polarity < 0 ):
+        st.write("Negative Comment")
+    else:
+        st.write("Neutral Comment")
+
+if st.button("check subjectivity"):
+    st.write(subjectivity)
+    if (subjectivity == 0):
+        st.write("Completely objective")
+    else:
+        st.write("Highly subjective")
